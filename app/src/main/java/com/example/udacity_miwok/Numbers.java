73f008877d426ee547ff7438905402fa71cf7832
missTinuade <estherjolugba@yahoo.com>
2019-10-08 09:16:53 +0000

    Initial commit

---
+++ app/src/main/java/com/example/udacity_miwok/Numbers.java
@@ -0,0 +1,14 @@
+package com.example.udacity_miwok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Numbers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+    }
+}
